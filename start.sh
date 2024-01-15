36db622b3c3d1c4f57d4015e19d4a5b64b22abfc
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-15 22:49:54 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 export PORT=${PORT:-8080}
 
-python start.py
+uwsgi --http 0.0.0.0:$PORT --gevent 1000 --http-websockets --master --wsgi-file main.py --callable app
 #gunicorn -b 0.0.0.0:$PORT --worker-class geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 --timeout 10000 main:app
