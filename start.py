3fedbff5aeaca1cd68444613d9591ada1d37826a
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-15 15:25:00 +0700

    update start.py

--- start.py
+++ start.py
@@ -1,4 +1,10 @@
+from main import app, sio as socketio
+import eventlet
+from eventlet import wsgi
 import os 
 
-os.system("chmod +x ./start.sh")
-os.system("./start.sh")
+PORT = os.getenv("PORT", 8080)
+
+if __name__ == '__main__':
+    socketio.attach(app, server=wsgi_server)
+    wsgi.server(eventlet.listen(('0.0.0.0', PORT)), app)
