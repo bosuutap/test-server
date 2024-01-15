b1e9e0520abcc61d423c9680b3c653109b8094eb
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-15 14:51:32 +0700

    update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/bash
 
-gunicorn -b 0.0.0.0:8080 --worker-class geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 main:app
+export PORT=${PORT:8080}
+gunicorn -b 0.0.0.0:PORT --worker-class geventwebsocket.gunicorn.workers.GeventWebSocketWorker -w 1 main:app
