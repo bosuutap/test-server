4b9f7cf15511f31230bcf6bd0edc846258fe8a09
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-15 15:28:58 +0700

    update start.py

--- start.py
+++ start.py
@@ -1,10 +1,15 @@
 from main import app, sio as socketio
-import eventlet
-from eventlet import wsgi
 import os 
 
 PORT = os.getenv("PORT", 8080)
 
 if __name__ == '__main__':
+    # Use eventlet as the worker for Waitress to support WebSockets
+    import eventlet
+    from eventlet import wsgi
+    # Create a WSGI server instance
+    wsgi_server = eventlet.listen(('0.0.0.0', PORT))
+    # Wrap the Flask app with Socket.IO
     socketio.attach(app, server=wsgi_server)
-    wsgi.server(eventlet.listen(('0.0.0.0', PORT)), app)
+    # Run the eventlet WSGI server
+    eventlet.wsgi.server(wsgi_server, app)
