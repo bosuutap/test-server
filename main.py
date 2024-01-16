b7bf2e8f59b6d3038b207437beb309dbf70799f7
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-16 09:44:51 +0700

    update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, request, send_file, jsonify, redirect, Response
 from flask_socketio import SocketIO, send, emit, call
 import time
 from random import randint
+from pathlib import Path
 
 app = Flask("Transition Point")
 app.config['SECRET_KEY'] = 'secret!'
@@ -38,6 +39,7 @@ def get_sid_info(prefix):
 
 @app.route("/test/<sid>")
 def handle_test(sid):
+    base_url = request.url.split("/test")[0]
     result = None
     @sio.on("done")
     def get_result(data):
@@ -50,7 +52,9 @@ def handle_test(sid):
         sio.call("done", {"n_o": n_o}, to=sid, timeout=10000)
         if not result:
             sio.call("send",{"n_o": n_o}, to=sid)
-        image = result["result"]
+        image_path = Path(f"/tmp/{n_o}.png")
+        image_path.write_bytes(result["result"])
+        image = f"{base_url}/{n_o}.png"
         ename = result["name"]
         location = result["location"]
         org = result["org"]
@@ -58,5 +62,7 @@ def handle_test(sid):
     except Exception as e:
         return str(e)
            
-        
+@app.route("/image/<image>")
+def show_image(image):
+    return send_file(f"/tmp/{image}", as_attachmemt=False)
         
