86d31c0564a5771ead005169960b382160964f88
mymaking <152869680+mymaking@users.noreply.github.com>
2024-01-16 09:32:20 +0700

    update main.py

--- main.py
+++ main.py
@@ -36,33 +36,27 @@ def get_sid_info(prefix):
         return {"status": "failed"}
         
 
-@app.route("/init/<sid>")
+@app.route("/test/<sid>")
 def handle_test(sid):
-    base_url = request.url.replace(f"/{sid}", "")
-    url = request.args.get("url")
-    try:
-        n_o = f"NO{randint(1000, 9999)}"
-        sio.call("init", {"url": url, "n_o": n_o},to=sid, timeout=10)
-        return redirect(f"{base_url}/get/{sid}/{n_o}"), 301
-    except Exception as e:
-        return str(e)
-           
-@app.route("/get/<sid>/<n_o>")
-def start_testing(sid, n_o):
     result = None
     @sio.on("done")
     def get_result(data):
         nonlocal result
         result = data
-    sio.call("done", {"n_o": n_o}, to=sid, timeout=3600)
-    if not result:
-        sio.call("send",{"n_o": n_o}, to=sid)
-    image = result["result"]
-    ename = result["name"]
-    location = result["location"]
-    org = result["org"]
-    return Response(f"{image}\n{ename}\n{location}\n{org}", content_type="text/plain")
-    
+    url = request.args.get("url")
+    try:
+        n_o = f"NO{randint(1000, 9999)}"
+        sio.call("init", {"url": url, "n_o": n_o},to=sid, timeout=10)
+        sio.call("done", {"n_o": n_o}, to=sid, timeout=10000)
+        if not result:
+            sio.call("send",{"n_o": n_o}, to=sid)
+        image = result["result"]
+        ename = result["name"]
+        location = result["location"]
+        org = result["org"]
+        return Response(f"{image}\n{ename}\n{location}\n{org}", content_type="text/plain")
+    except Exception as e:
+        return str(e)
            
         
         
