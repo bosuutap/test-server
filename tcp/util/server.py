fd62222b5a839eb826d4f2445d614255ae315320
chantroi <tonglanhchua@gmail.com>
2024-02-27 14:25:56 +0700

    update tcp/util/server.py

--- tcp/util/server.py
+++ tcp/util/server.py
@@ -5,21 +5,21 @@ class TCPServer:
     def __init__(self, host, port):
         self.host = host
         self.port = port
-        self.is_running = False
+        self.running = False
         self.server_socket = None
 
     def start(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(5)
-        self.is_running = True
+        self.running = True
 
-        while self.is_running:
+        while self.running:
             client_socket, client_address = self.server_socket.accept()
             # Xử lý kết nối từ client
 
     def stop(self):
-        self.is_running = False
+        self.running = False
         if self.server_socket:
             self.server_socket.close()
 
