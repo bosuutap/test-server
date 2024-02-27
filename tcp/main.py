bd08604be2581f7ec9c53a9bc548b71deea4d458
chantroi <tonglanhchua@gmail.com>
2024-02-27 17:22:47 +0700

    update tcp/main.py

--- tcp/main.py
+++ tcp/main.py
@@ -1,10 +1,13 @@
 import socket
 import os
-from util import run_tcp, lite
+from util import lite
 from threading import Thread
 
 port = os.getenv("PORT", 8080)
-server = run_tcp(port)
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(("0.0.0.0", port))
+server.listen()
+print("Server is running")
 
 def polling():
     while server.running:
@@ -12,7 +15,7 @@ def polling():
         bytes_data = conn.recv(10*1024*1024)
         text = bytes_data.decode('utf-8', errors='replace')
         result = lite(text).encode('utf-8', errors='replace')
-        server.send(result)
+        conn.send(result)
         
 Thread(target=polling).start()
 while True:
