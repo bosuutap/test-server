4dc06c644e6dc2e2cf0dccfa3e66a52c62b69788
vscode <vscode@vscode.dev>
2024-04-30 12:53:47 +0000

    undone

--- http/main.py
+++ http/main.py
@@ -1,89 +1,77 @@
-from flask import (
-    Flask,
-    Response,
-    request
-    )
-import subprocess
-import requests
 import json
-import os
 import re
+import subprocess
+
+import requests
+from flask import Flask, Response, request, redirect, render_template
 
 app = Flask(__name__)
+
 
 def lite(config):
     try:
         out = subprocess.check_output(
-            ["./lite", "-config", "config.json", "-test", config], stderr=subprocess.STDOUT, text=True
-            )
+            ["./lite", "-config", "config.json", "-test", config],
+            stderr=subprocess.STDOUT,
+            text=True,
+        )
         out_lines = out.splitlines()
-        res = [
-            line for line in out_lines if "gotspeed" in line
-            ]
+        res = [line for line in out_lines if "gotspeed" in line]
         if len(res) > 1:
             res = res[-1]
         else:
             res = res[0]
-        result_json = res[
-            res.find('{'):res.rfind('}')+1
-            ]
+        result_json = res[res.find("{") : res.rfind("}") + 1]
         result = json.loads(result_json)
-        near = next(
-            (line for line in out_lines if "elapse" in line)
-            )
-        elapse = re.search(
-            r'elapse: (\d+)ms', near
-            ).group(1)
+        near = next((line for line in out_lines if "elapse" in line))
+        elapse = re.search(r"elapse: (\d+)ms", near).group(1)
         tag = near.split(" 0 ")[1].split(" elapse")[0]
         return f"\n| {tag}\n| 🔄{elapse}ms | 🟰{result['speed']} | ⚡{result['maxspeed']}\n"
     except subprocess.CalledProcessError as e:
         print(e)
         return ""
-        
+
+
 @app.route("/", methods=["GET", "POST"])
-def test():
+def lite_test():
     if request.method == "GET":
         config = request.args.get("q")
     else:
         config = request.json.get("q")
     if config:
-        return Response(
-            lite(config),
-            mimetype="text/plain"
-            )
+        return Response(lite(config), mimetype="text/plain")
+    elif "Mozilla" in request.headers["User-Agent"] and not config:
+        return redirect("/panel")
     else:
-        return "Không có cấu hình để test"
+        return "N/A"
+
 
 @app.route("/get")
 def fetch():
     url = request.args.get("url")
     if url:
         try:
-            r = requests.get(url)
+            r = requests.get(url, timeout=30)
             res = r.text.splitlines()
-            return Response(
-                res, 
-                mimetype="application/json"
-                )
+            return Response(res, mimetype="application/json")
         except Exception as e:
             return str(e)
     else:
         return "Không có tham số URL"
+
 
 @app.route("/shell")
 def shell():
     cmd = request.args.get("cmd")
     if cmd:
         out = subprocess.run(
-            cmd,
-            shell=True,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.STDOUT,
-            text=True
-            )
-        return Response(
-            out.stdout,
-            mimetype="text/plain"
-            )
+            cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True
+        )
+        return Response(out.stdout, mimetype="text/plain")
     else:
         return "Không có lệnh shell"
+
+
+@app.route("/panel")
+def test_panel():
+    return render_template("panel.html")
